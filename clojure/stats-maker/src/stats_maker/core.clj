e08cd101dc20f90d429f14959c8fdd04185be828
Robert Rees <robert@wazoku.com>
2013-01-13 14:34:22 +0000

    Creates a working hello world jar

---
+++ clojure/stats-maker/src/stats_maker/core.clj
@@ -0,0 +1,6 @@
+(ns stats-maker.core
+	(:gen-class))
+
+(defn -main
+  [& args]
+  (println "Hello, World!"))
---
+++ clojure/stats-maker/test/stats_maker/core_test.clj
@@ -0,0 +1,7 @@
+(ns stats-maker.core-test
+  (:use clojure.test
+        stats-maker.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
